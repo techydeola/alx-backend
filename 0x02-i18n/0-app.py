dbf75eb5d7a905c4020707e151e151737ae3f24d
Johnson adeola <johnsamxy@gmail.com>
2024-04-09 18:02:24 +0100

    quick fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """
         a simple function tahat returns a template
