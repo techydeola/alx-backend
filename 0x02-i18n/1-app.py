55a3af78bb3063722b09eb19adffcae4e85c8cbb
Johnson adeola <johnsamxy@gmail.com>
2024-04-09 18:57:21 +0100

    new task

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,8 +5,21 @@
 
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+        a simple Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
@@ -14,7 +27,7 @@ def index():
     """
         a simple function tahat returns a template
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
